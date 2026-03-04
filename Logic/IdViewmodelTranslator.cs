bbcaf2cc156c034ea0cec19ff92615c632bd699f
SubmarineCoder <igorilic68@gmail.com>
2026-03-04 20:03:26 +0100

    static class for IdModel to IdViewmodel translation

---
+++ Logic/IdViewmodelTranslator.cs
@@ -0,0 +1,19 @@
+﻿namespace Schulbibliothek.Logic
+{
+    public static class IdViewmodelTranslator
+    {
+        public static int IdEncode(int id)
+        {
+            id += 69;
+
+            return id;
+        }
+
+        public static int IdDecode(int id)
+        {
+            id += 69;
+
+            return id;
+        }
+    }
+}
